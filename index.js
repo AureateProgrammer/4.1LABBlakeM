420bcac5c3149e3b5f969f88b6f47b94855381db
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-03-19 07:43:31 -0400

    Fixed bug

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function caculateTotalCost(price, quantity, taxRate,discount) {
-    let totalCost = (price * quantity) * (1 + taxRate) (discount / price );
+    let totalCost = (price * quantity) * (1 + taxRate) - (discount / price );
 return totalCost;
 }
 
-console.log(caculateTotalCost(10,14,1))
+console.log(caculateTotalCost(10,14,1,5))
 
 
 function checkEligibility(age, isEmployed){
