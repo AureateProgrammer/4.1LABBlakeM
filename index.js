34ce25e9afcc7b36d77bbe426f3ba6a83bd75880
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-03-19 07:38:36 -0400

    Finishing Function

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 function caculateTotalCost(price, quantity, taxRate) {
     let totalCost = (price * quantity) * (1 + taxRate);
-return totalCost
+return totalCost;
 }
 
 console.log(caculateTotalCost(10,14,1))
 
-function checkEligblity(age,isEmployed){
-    while 
+function checkEligibility(age, isEmployed){
+    if (age <= 18){
+        console.log("Not Eligible");
+    }
+    else if (isEmployed){
+        console.log("Eligible");
+    }
+    else{
+        console.log("Conditionally Eligible");
+    }
 }
+
+console.log(checkEligibility(18, false))
+console.log(checkEligibility(19, true))
+console.log(checkEligibility(25, false))
