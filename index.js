7621372422594d75d35cf026a7b73321f737f564
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-03-19 07:26:15 -0400

    Creating function for task two

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function caculateTotalCost(price, quantity, taxRate) {
+    let totalCost = (price * quantity) * (1 + taxRate);
+return totalCost
+}
 
+console.log(caculateTotalCost(10,14,1))
+
+function checkEligblity(age,isEmployed){
+    while 
 }
